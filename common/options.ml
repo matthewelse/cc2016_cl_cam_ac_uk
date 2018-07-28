d8e2712b704bf3ef7d1d8b8b144ec6bf9975593a
Matthew Else <matthewelse1997@gmail.com>
2018-07-28 23:57:26 +0100

    Tidying

--- common/options.ml
+++ common/options.ml
@@ -1,8 +1,6 @@
-
-type t = {
-    verbose_front : bool
-  ; verbose_tree : bool
-  ; verbose_back : bool
-  ; stack_max : int
-  ; heap_max : int
-  }
+type t =
+  { verbose_front: bool
+  ; verbose_tree: bool
+  ; verbose_back: bool
+  ; stack_max: int
+  ; heap_max: int }
